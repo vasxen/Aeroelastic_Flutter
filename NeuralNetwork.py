3396aed57d7243f18dc4600efec19b82c39e5e4b
Vasileios Xenodochidis <vasxen2001@gmail.com>
2024-07-14 12:31:41 +0300

    reqorked Neural network to better fit regression

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,69 +1,102 @@
-import tensorflow
-import keras
-import pandas as pd
-import keras_tuner as kt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 import numpy as np
-from numpy.typing import NDArray
+import pandas as pd
+import seaborn as sb
+# import tensorflow as tf
+import plotly.graph_objects as go
+import keras
+import keras_tuner as kt
 
-## Read Data
+def plot_loss(history):
+  y1 = history.history['loss']
+  y2 = history.history['val_loss']
+  x = list(range(1, len(y1) +1))
+  Loss = go.Scatter(x = x, y = y1, mode = 'lines+markers', name = 'loss')
+  ValLoss = go.Scatter(x = x, y = y2, mode = 'lines+markers', name = 'validation loss')
+  fig = go.Figure()
+  fig.add_trace(Loss)
+  fig.add_trace(ValLoss)
 
-Data:pd.DataFrame = pd.read_excel('FunctionDataPoints.xlsx')
-Data = Data.drop(['Y2'], axis = 1)
-scaler = MinMaxScaler()
-transform = scaler.fit(Data)
+  fig.update_layout(xaxis_title = 'Epoch', yaxis_title = 'Error [FlutterVelocity]')
+  fig.show()
 
-Data_tr = transform.transform(Data)
+def Preprocessing(dataset: pd.DataFrame, train_test_ratio:float = 0.9):
+  dataset.pop('Y2')
+  train = dataset.sample(frac = train_test_ratio, random_state = 44)
+  test = dataset.drop(train.index)
+  sb.pairplot(train[['X0', 'X1', 'X2', 'X3', 'Y1']])
+  plt.show()
+  print(train.describe().transpose())
 
-X = Data_tr[:,0:4]
-Y = Data_tr[:,4:]
+  train_features = train.copy()
+  test_features = test.copy()
 
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size = 0.1, random_state = 55) #type: ignore
-TrainingPoints = Xtrain.shape[0]
-TestPoints = Xtest.shape[0]
+  train_labels = train_features.pop('Y1')
+  test_labels = test_features.pop('Y1')
+
+  return train_features, train_labels, test_features, test_labels
+
+def staticmodel(normalizer: keras.Layer):
+	model = keras.Sequential([
+    normalizer,
+    keras.layers.Dense(64, activation= 'relu'),
+    keras.layers.Dense(64, activation= 'relu'),
+    keras.layers.Dense(64, activation= 'relu'),
+    keras.layers.Dense(1)
+  ])
+	model.compile(loss= 'mean_absolute_error', optimizer= keras.optimizers.Adam(0.001), metrics= ['mse' ,'mae']) #type: ignore
+	return model
 
 
-def model_builder(hp: kt.HyperParameters) -> keras.Model:
-    model = keras.models.Sequential()
-    # Architecture hyperparameters
-    NLayers: int = hp.Int('Number_of_Layers', min_value= 1, max_value=5, step= 1) #type: ignore
-    
-    for i in range(NLayers):
-        NNodes = hp.Choice(f'units L{i+1}', [2, 4, 8, 16, 32])
-        activation = hp.Choice(f'activation L{i+1}', ['relu', 'tanh'])
-        model.add(keras.layers.Dense(units= NNodes, activation = activation, name = f'Hidden_{i+1}'))
+def main():
+    def modelbuilder(hp: kt.HyperParameters):
+        model = keras.Sequential()
+        model.add(normalizer)
+
+        NUMBER_LAYERS = hp.Int('N_Hidden_layers', 1, 10, 1)
+        for i in range(NUMBER_LAYERS): #type: ignore
+            UNITS = hp.Choice(f'Units L{i+1}', [2, 4, 8, 16, 32, 64, 128, 256, 512])
+            model.add(keras.layers.Dense(UNITS, activation = 'relu'))
         
-    model.add(keras.layers.Dense(1, activation= 'linear', name = 'Output'))
+        model.add(keras.layers.Dense(1))
+        LR = hp.Choice('learning rate', [1e-2, 1e-3, 1e-4])
+        model.compile(loss= 'mean_absolute_error', optimizer= keras.optimizers.Adam(LR), metrics= ['mse' ,'mae']) #type: ignore
+        return model
+  
+    raw_dataset = pd.read_excel('FunctionDataPoints.xlsx')
+    train_features, train_labels, test_features, test_labels = Preprocessing(raw_dataset)
 
-    # compilation hyperparameters
-    lr = hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4])
-    optimizer = keras.optimizers.Adam(learning_rate= lr) #type: ignore
-    model.compile(optimizer = optimizer, #type: ignore
-                      metrics = ['accuracy', 'mse'],
-                      loss = 'mean_squared_error')
+    # Data normalization
+    normalizer = keras.layers.Normalization(axis = -1)
+    normalizer.adapt(np.array(train_features))
+
+    # model = staticmodel(normalizer)
+
+    # train_history = model.fit(train_features, train_labels, validation_split=0.1, verbose= '2', epochs=100)
+    # model.save('1Hiddenlayers.keras')
+    # eval = model.evaluate(test_features, test_labels)
+    # print(eval)
+    # plot_loss(train_history)
+    # print('===================================================')
+
+    ## Tune Model
+    tuner = kt.Hyperband(modelbuilder,
+                objective= 'val_mae',
+                max_epochs = 80,
+                directory = 'C:/Users/vasxen/OneDrive/Thesis',
+                project_name = 'Hypermodel_Tuning2')
     
-    return model
+    tensorboard = keras.callbacks.TensorBoard('C:/Users/vasxen/OneDrive/Thesis/tb_logs')
+    tuner.search(train_features, train_labels, validation_data = (test_features, test_labels), callbacks= [tensorboard])
 
-tuner = kt.Hyperband(model_builder, 
-                    objective= 'val_accuracy',
-                    max_epochs= 30,
-                    factor=  3,
-                    directory='my_dir',
-                    project_name='intro_to_kt',
-                    overwrite = True)
+    bestmodel = tuner.get_best_models(1)[0]
+    bestmodel.save('tunedmodel2.keras')
+    print('Tuned model Info')
+    print(bestmodel.summary())
+    print(bestmodel.evaluate(test_features, test_labels))
 
-stopearly = keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 10)
-tensorboard = keras.callbacks.TensorBoard('/tmp/tb_logs')
-tuner.search(Xtrain, Ytrain, epochs = 50, validation_split = 0.1, verbose=2)
+if __name__ == '__main__':
+    main()
 
-best_params: kt.HyperParameters = tuner.get_best_hyperparameters(num_trials=1)[0]
-
-Finalmodel = tuner.hypermodel.build(best_params) #type: ignore
-history = Finalmodel.fit(Xtrain, Ytrain, epochs = 50 , validation_split = 0.1)
-
-LossValue, accuracy, mse = Finalmodel.evaluate(Xtest, Ytest)
-print('============ FINAL MODEL TEST RESULTS =================')
-Finalmodel.summary()
-print('Accuracy: %.2f' % (accuracy*100))
-print('MSE: %.2f' % (mse))
+# Run this command to start tensorboard
+# python -m tensorboard.main --logdir=tb_logs
